4362ce60c278c61f47d41770d729bcce654385d5
Vincent-von-Schmidt <vincentvonschmidt@gmail.com>
2020-06-12 08:08:15 +0200

    new file:   Hauptprogramm/Hauptprogramm.ino 	new file:   Hauptprogramm/fahren.cpp 	new file:   Hauptprogramm/fahren.h 	new file:   Hauptprogramm/meinePins.h 	new file:   Hauptprogramm/messen.cpp 	new file:   Hauptprogramm/messen.h

---
+++ Hauptprogramm/Hauptprogramm.ino
@@ -0,0 +1,230 @@
+#include <SPI.h>
+#include "meinePins.h"
+#include "messen.h"
+#include "fahren.h"
+#include <EEPROM.h>
+#define V 80
+#define WEISS 90
+#define SCHWARZ 25
+
+#define testeWeiss ((WerteW[RECHTS] >= WEISS) && (WerteW[LINKS] >= WEISS))
+#define testeSchwarz ((WerteW[RECHTS] <= SCHWARZ) && (WerteW[LINKS] <= SCHWARZ))
+#define testeLinie  ((WerteW[RECHTS] <= 50) || (WerteW[LINKS] <= 50))
+
+#define WEISS_LUECKE 90
+#define testeWeiss_Lueke ((WerteW[RECHTS] >= WEISS_LUECKE) && (WerteW[LINKS] >= WEISS_LUECKE))
+
+int trigger = 7;
+int echo = 8;
+int dauer = 0;
+long entfernung = 0;
+
+
+void LichtTest() {
+  messeLicht();
+  LichtUmrechnen();
+  zeigeLichtSeriell();
+  delay(200);
+  return;
+}
+void setup() {
+  Serial.begin(115200);
+  hardwareInit();
+  // Serial.println("Init ok");
+
+  LeseMinMax();
+  zeigeMinMax ();
+
+  int taste = leseTaster();
+  while (taste == 0)
+    taste = leseTaster();
+
+  if (taste == Taster_L) {
+    Serial.println("Kalibrierung...");
+    kalibrieren();
+    zeigeMinMax ();
+    SpeichereMinMax();
+    Serial.println("Kalibriert");
+
+  }
+  delay(1500);
+  pinMode(trigger, OUTPUT);
+  pinMode(echo, INPUT);
+}
+
+void Entfernung (){
+   digitalWrite(trigger, LOW);
+  delay(5);
+  digitalWrite(trigger, HIGH);
+  delay(10);
+  digitalWrite(trigger, LOW);
+  dauer = pulseIn(echo, HIGH);
+  entfernung = (dauer / 2) / 29.1;
+  Serial.print(entfernung);
+  Serial.println(" cm");
+  
+}
+
+void loop() {
+OnFwd(OUT_AB,V);
+
+ Entfernung ();
+
+ if (entfernung < 11) {
+Off(OUT_AB);
+delay (2000);
+}
+  /*
+    //fahreZeitBisSchwarz(3000, 255);
+    // delay(2000);
+    //dreheZeitBisSchwarz(1000, V, RECHTS);
+    long t1 = millis();
+    long t2 = t1;
+    OnFwd(OUT_AB, 80);
+
+    messeLicht();
+
+    while ((t2 - t1) < 3000) {
+      messeLicht();
+      LichtUmrechnen();
+      zeigeLichtSeriell();
+      t2 = millis();
+      int diff = (10 * (WerteW[RECHTS] - WerteW[LINKS])) / 2;
+      OnFwd(OUT_A, V + diff);
+      OnFwd(OUT_B, V - diff);
+    }
+
+    Off (OUT_AB);
+
+    while (1);
+
+    return;*/
+  /* messeLicht();
+    LichtUmrechnen();
+    if (leseTaster()) {
+     umfahreDose(RECHTS);
+    }
+
+
+    //zeigeLichtSeriell();
+
+
+    if (testeWeiss) {          //Lücke
+     long t1;
+     Serial.println("Weiss");
+     OnFwd(OUT_AB, V);
+     delay(100);
+     messeLicht();
+     LichtUmrechnen();
+     t1 = millis();
+     while (((millis() - t1) < 2500) && (testeWeiss)) {
+       messeLicht();
+       LichtUmrechnen();
+       if (!testeWeiss) {
+         Serial.println("not Weiss");
+         return;
+       }
+       // else steht immer noch auf weiss
+     }
+     Serial.println("kein Linie");
+     //Off(OUT_AB);
+     //delay(1000);
+
+     // Linie verloren, rückwärts
+     OnRev(OUT_AB, V);
+
+     t1 = millis();
+     while (((millis() - t1) < 2700) && (testeWeiss)) {
+       messeLicht();
+       LichtUmrechnen();
+     }
+     delay(100);
+     Off(OUT_AB);
+     return;
+     //delay(1000);
+     /*
+         messeLicht();
+         LichtUmrechnen();
+         if (testeWeiss_Lueke) {
+           // Off(OUT_AB);
+           Serial.println("Luecke");
+           // delay(1000);
+           return;
+         } else {
+           Serial.println("keine Luecke");
+           return;
+         }
+         //delay(1000);
+  */
+
+  /* //Luecke oder Linie verloren
+     long zeit = 2000;
+     long t1 = millis();
+     long t2 = t1;
+     messeLicht();
+     LichtUmrechnen();
+
+     // Anhalten
+     Off(OUT_AB);
+     delay(1000);
+
+     // Zurueckfahren bis Schwarz auf 1/2 (?) Sensoren
+     OnRev(OUT_AB, 40);
+     while ((WerteW[RECHTS] > SCHWARZ) || (WerteW[LINKS] > SCHWARZ)) {
+      messeLicht();
+      LichtUmrechnen();
+      //zeigeLichtSeriell();
+     }
+     Off(OUT_AB);
+     delay(1000);
+     //  --> Entscheiden, ob Lücke oder Linie verloren
+     // Linienende erkannt ->Lücke
+     if ((WerteW2[RECHTS] < SCHWARZ) || (WerteW2[LINKS] < SCHWARZ)) {
+      Serial.println("keine Lücke");
+      return;
+     } else {
+      Serial.println("Lücke");
+      // optional ausrichten
+      messeLicht();
+      LichtUmrechnen();
+      OnFwd(OUT_A, 50);
+      OnRev(OUT_B, 50);
+     }
+     do {
+      messeLicht();
+      LichtUmrechnen();
+     } while (WerteW[RECHTS] && WerteW[LINKS] < SCHWARZ);
+     Off(OUT_AB);
+     // Vorfahren bis nächste schwarze Linie (begrenzt auf festgelegte Strecke)
+     messeLicht();
+     LichtUmrechnen();
+     OnFwd(OUT_AB, V);
+     while (WerteW[RECyHTS] && WerteW[LINKS] < SCHWARZ) {
+      messeLicht();
+      LichtUmrechnen();
+     }
+     // Linie gefunden -> return
+     Off(OUT_AB);
+     Serial.println("Linie gefunden");
+    return;*/
+
+
+
+/*
+else {
+  // Kreuzung
+  if (WerteW2[RECHTS] < 30 && WerteW2[LINKS] < 30) {
+    OnFwd(OUT_A, -100);                               //+180=Links; -100=Rechts
+    OnFwd(OUT_B, +180);                               //-100=Links; +180=Rechts
+    delay(130);
+  }
+
+  else {
+    // Linienfolger
+    int diff = (13 * (WerteW2[RECHTS] - WerteW2[LINKS])) / 2;
+    OnFwd(OUT_A, V + diff);
+    OnFwd(OUT_B, V - diff);
+
+  }
+}
+*/}
---
+++ Hauptprogramm/messen.cpp
@@ -0,0 +1,223 @@
+#include <Arduino.h>
+#include "meinePins.h"
+#include "messen.h"
+#include <SPI.h>
+#include <EEPROM.h>
+
+#define MAX_ADC 1023
+
+byte LEDs[ANZ_LEDs] = {LED_W, LED_W2};
+byte LS[ANZ_LS] = {SE_LI, SE_RE};
+int WerteW[ANZ_LS];
+
+int WerteW2[ANZ_LS];
+//int WerteG[ANZ_LS];
+//int WerteR[ANZ_LS];
+int MinW[ANZ_LS];
+//int MinG[ANZ_LS];
+//int MinR[ANZ_LS];
+int MaxW[ANZ_LS];
+//int MaxG[ANZ_LS];
+//int MaxR[ANZ_LS];
+int MinW2[ANZ_LS];
+int MaxW2[ANZ_LS];
+
+
+void messeLicht() {
+  byte pause = 2;
+  setLED (LED_W);
+  delay(pause);
+  for (int i = 0; i < ANZ_LS; i++) {
+    WerteW[i] = analogRead(LS[i]);
+  }
+  setLED (LED_W2);
+  delay(pause);
+  for (int i = 0; i < ANZ_LS; i++) {
+    WerteW2[i] = analogRead(LS[i]);
+  }
+  /* setLED (LED_G);
+    delay(pause);
+    for (int i = 0; i < ANZ_LS; i++) {
+     WerteG[i] = analogRead(LS[i]);
+    }
+    setLED (LED_R);
+    delay(pause);
+    for (int i = 0; i < ANZ_LS; i++) {
+     WerteR[i] = analogRead(LS[i]);
+    }*/
+  setLED(0);
+}
+
+void zeigeLichtSeriell() {
+  //Serial.print ("W_li: ");
+  Serial.print(WerteW2[0]);
+  Serial.print(" ");
+  Serial.print(WerteW[0]);
+  Serial.print(" ");
+  //Serial.print(WerteG[0]);
+  //Serial.print(" ");
+  //Serial.print(WerteR[0]);
+  //Serial.print(" \t W_re: ");
+  //Serial.print(" ");
+  Serial.print(WerteW[1]);
+  Serial.print(" ");
+  Serial.print(WerteW2[1]);
+  //Serial.print(WerteG[1]);
+  //Serial.print(" ");
+  //Serial.print(WerteR[1]);
+  Serial.println();
+}
+
+void kalibrieren() {
+  for (int j = 0; j < ANZ_LS; j++) {
+    MinW[j] = MAX_ADC;
+    MinW2[j] = MAX_ADC;
+    //   MinG[j] = MAX_ADC;
+    //   MinR[j] = MAX_ADC;
+    MaxW[j] = 0;
+    MaxW2[j] = 0;
+    //   MaxG[j] = 0;
+    //  MaxR[j] = 0;
+  }
+  for (int i = 0; i < 500; i++) {
+    messeLicht();
+    for (int j = 0; j < ANZ_LS; j++) {
+
+
+      if (WerteW[j] > MaxW[j])
+        MaxW[j] = WerteW[j];
+      if (WerteW[j] < MinW[j])
+        MinW[j] = WerteW[j];
+
+      if (WerteW2[j] > MaxW2[j])
+        MaxW2[j] = WerteW2[j];
+      if (WerteW2[j] < MinW2[j])
+        MinW2[j] = WerteW2[j];
+      /*
+
+            if (WerteG[j] > MaxG[j])
+              MaxG[j] = WerteG[j];
+            if (WerteG[j] < MinG[j])
+              MinG[j] = WerteG[j];
+
+            if (WerteR[j] > MaxR[j])
+              MaxR[j] = WerteR[j];
+            if (WerteR[j] < MinR[j])
+              MinR[j] = WerteR[j];
+      */
+    }
+  }
+}
+
+void zeigeMinMax () {
+  Serial.print ("Min_li: ");
+  Serial.print(MinW[0]);
+  Serial.print(" ");
+  Serial.print(MinW2[0]);
+  /*
+    Serial.print(" ");
+    Serial.print(MinG[0]);
+    Serial.print(" ");
+    Serial.print(MinR[0]);
+  */
+  Serial.print(" \t Min_re: ");
+  Serial.print(MinW[1]);
+  Serial.print(" ");
+  Serial.print(MinW2[1]);
+  /*
+    Serial.print(" ");
+    Serial.print(MinG[1]);
+    Serial.print(" ");
+    Serial.print(MinR[1]);
+  */
+  Serial.println();
+
+  Serial.print ("Max_li: ");
+  Serial.print(MaxW[0]);
+  Serial.print(" ");
+  Serial.print(MaxW2[0]);
+
+  /*
+    Serial.print(" ");
+    Serial.print(MaxG[0]);
+    Serial.print(" ");
+    Serial.print(MaxR[0]);
+  */
+  Serial.print(" \t Max_re: ");
+  Serial.print(MaxW[1]);
+  Serial.print(" ");
+  Serial.print(MaxW2[1]);
+  /*
+    Serial.print(MaxG[1]);
+    Serial.print(" ");
+    Serial.print(MaxR[1]);
+  */
+  Serial.println();
+}
+void LichtUmrechnen () {
+  for (int j = 0; j < ANZ_LS; j++) {
+    WerteW[j] =   map (WerteW[j], MinW[j], MaxW[j], 0, 100);
+    WerteW2[j] =   map (WerteW2[j], MinW2[j], MaxW2[j], 0, 100);
+    //WerteG[j] =   map (WerteG[j], MinG[j], MaxG[j], 0, 100);
+    //WerteR[j] =   map (WerteR[j], MinR[j], MaxR[j], 0, 100);
+  }
+
+}
+
+void SpeichereMinMax() {
+  EEPROM.put( 0, MinW[0]);
+  //EEPROM.put( 2, MinG[0]);
+  //EEPROM.put( 4, MinR[0]);
+  EEPROM.put( 6, MaxW[0]);
+  //EEPROM.put( 8, MaxG[0]);
+  //EEPROM.put(10, MaxR[0]);
+  EEPROM.put(12, MinW[1]);
+  //EEPROM.put(14, MinG[1]);
+  //EEPROM.put(16, MinR[1]);
+  EEPROM.put(18, MaxW[1]);
+  //EEPROM.put(20, MaxG[1]);
+  //EEPROM.put(22, MaxR[1]);
+  EEPROM.put( 24, MinW2[0]);
+  EEPROM.put( 26, MinW2[1]);
+  EEPROM.put( 28, MaxW2[0]);
+  EEPROM.put( 30, MaxW2[1]);
+}
+
+
+void LeseMinMax() {
+  EEPROM.get( 0, MinW[0]);
+  //EEPROM.get( 2, MinG[0]);
+  //EEPROM.get( 4, MinR[0]);
+  EEPROM.get( 6, MaxW[0]);
+  // EEPROM.get( 8, MaxG[0]);
+  //EEPROM.get(10, MaxR[0]);
+  EEPROM.get(12, MinW[1]);
+  //EEPROM.get(14, MinG[1]);
+  //EEPROM.get(16, MinR[1]);
+  EEPROM.get(18, MaxW[1]);
+  //EEPROM.get(20, MaxG[1]);
+  //EEPROM.get(22, MaxR[1]);
+  EEPROM.get( 24, MinW2[0]);
+  EEPROM.get( 26, MinW2[1]);
+  EEPROM.get( 28, MaxW2[0]);
+  EEPROM.get( 30, MaxW2[1]);
+
+}
+
+
+void hardwareInit () {
+  pinMode(LICHT_CS, OUTPUT);
+  digitalWrite(LICHT_CS, HIGH);
+  SPI.begin();
+
+  /*Serial.println("Hallo Herr Krause.");
+    Serial.println("Hardware Init");*/
+}
+
+
+
+/*void erkenneWeiss() {
+  if (WerteW[RECHTS] && WerteW[LINKS] > WEISS) {
+    Off(OUT_AB);
+  }
+}*/
